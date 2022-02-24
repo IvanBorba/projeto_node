edbe31cbb3772ebd7e58f00e1f6228bec6539c80
Patrick Nekel <patrick@kenzie.com.br>
2022-02-24 10:50:28 -0300

    :sparkles: add get users

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
-import express from "express"
-import { v4 as uuidv4} from "uuid"
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
-const app = express()
+const app = express();
 app.use(express.json());
 
-
-const users = []
-
+const users = [];
 
 app.post("/user", (request, response) => {
- console.log(request.body)
- const {email, name} = request.body
- const userArealdyExists =  users.find((user) => user.email === email)
+  console.log(request.body);
+  const { email, name } = request.body;
+  const userArealdyExists = users.find((user) => user.email === email);
 
- if(userArealdyExists) {
-  return response.status(400).json({error: "This email address is already being used"})
- }
- 
- users.push({
-  email,
-  name,
-  id: uuidv4()
-  
- })
+  if (userArealdyExists) {
+    return response
+      .status(400)
+      .json({ error: "This email address is already being used" });
+  }
 
- return response.status(201).send()
+  users.push({
+    email,
+    name,
+    id: uuidv4(),
+  });
 
-})
+  return response.status(201).send();
+});
 
-app.get("/", (request, response) => { 
- return response.json({
-  message: "Hello World Kenzie"
- })
-})
+app.get("/users", (request, response) => {
+  return response.json(users);
+});
 
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Hello World Kenzie",
+  });
+});
 
-
-
-
-app.listen(3333)
+app.listen(3333);
